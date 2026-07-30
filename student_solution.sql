d4bc220057ddc8670ce4d9d6e0efebef4aefc84a
premalathas1 <premalathas@rvsgroup.com>
2026-07-30 14:31:04 +0530

    Create student_solution.sql

---
+++ student_solution.sql
@@ -0,0 +1,17 @@
+-- ==========================================
+-- CollegeDB Assignment
+-- Student Name:
+-- Register Number:
+-- ==========================================
+
+-- Step 1: Create Database
+
+
+-- Step 2: Select Database
+
+
+-- Step 3: Create Department Table
+
+
+-- Step 4: Display Table Structure
+
